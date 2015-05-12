be34455c8208c7670f6e1ced882422927897b612
Matthias Larsen <mlarsen@gammamedia.org>
2015-05-12 17:04:45 +0200

    Various frontend changes. Added classifier

---
+++ classifier.py
@@ -0,0 +1,61 @@
+def classScore(comment, Author_Name, UCT_Time, Thread_Score, External_link, model): #comment?
+
+	AFINN_path = os.getcwd() + '/AFINN/AFINN-111form.csv'
+	import csv
+	import datetime
+	import numpy as np
+	#from sklearn.ensemble import RandomForestClassifier
+
+	ANEW_words = {}
+	with open(AFINN_path) as f:
+		try:
+			reader = csv.reader(f, delimiter=';')
+			for row in reader:
+				try:
+					ANEW_words[row[0]]=row[1] # Each element in the csv row, is a word
+				except:
+					print("empty end field")
+		except:
+			pass
+
+	attributeNames = ["comment_length", "sentiment_Score"
+						  "Author_name_length", "UCT Timestamp", "time of day", "Day of week",
+						  "Hour of day", "Thread Score", "External Link"]
+
+	X_input = np.empty([1, len(attributeNames)+1])
+
+	X_input[0,0] =len(comment) # Length of comment
+
+	for words in ANEW_words:
+		X_input[0,1] += comment.count(words) * int(ANEW_words[words])
+
+	X_input[0,2] = len(Author_Name) # Author_Name_Length
+
+	X_input[0,3] = UCT_Time
+
+	tempTime = datetime.datetime.utcfromtimestamp(X_input[0,4]).hour-5.5
+	if tempTime <= 0: # roll over til cat coding 5
+		X_input[0,4] = 5
+	elif  tempTime <= 4: # Early morning
+		X_input[0,4] = 0
+	elif tempTime <= 8: # morning to middle of day and not early
+		X_input[0,4] = 1
+	elif tempTime <= 12: # morning to middle of day
+		X_input[0,4] = 2
+	elif tempTime <= 16: # Late afternoon
+		X_input[0,4] = 3
+	elif tempTime <= 20: # Early eevening
+		X_input[0,4] = 4
+	else:
+		X_input[0,4] = 5 # Late evening to night
+
+	X_input[0,5] = datetime.datetime.utcfromtimestamp(X_input[0,4]-19800).weekday()
+
+	X_input[0,6] = tempTime+5.5
+
+	X_input[0,7] = Thread_Score
+
+	X_input[0,8] = External_link # 0 or 1
+
+	prediction = model.predict(X_input)
+	return prediction
--- server.py
+++ server.py
@@ -2,48 +2,82 @@ import tornado.ioloop
 import tornado.web
 import json
 import os
+import numpy as np
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
 from import_data import reddit_info
+from classifier import classScore
+from datetime import datetime
+import calendar
 
 class DataHandler(tornado.web.RequestHandler):
-        def get(self):
-                self.set_header('Content-Type', 'application/json')
-                self.set_header('Access-Control-Allow-Origin', '*')
-                self.write(json.dumps(comments))
+	def get(self):
+		self.set_header('Content-Type', 'application/json')
+		self.set_header('Access-Control-Allow-Origin', '*')
+		self.write(json.dumps(comments))
 
 class ScoringHandler(tornado.web.RequestHandler):
-        def post(self):
-                data = json.loads(self.request.body.decode('utf-8'))
-                self.write("Hello, world: " + data["comment"])
+	def post(self):
+		data = json.loads(self.request.body.decode('utf-8'))
+
+		self.set_header('Content-Type', 'application/json')
+		self.set_header('Access-Control-Allow-Origin', '*')
+
+		d = datetime.utcnow()
+		unixtime = calendar.timegm(d.utctimetuple())
+		score = classScore(data["comment"], data["username"], unixtime, data["threadscore"], data["external"], clfrand)
+
+		self.write("Hello, world: " + score)
 
 class WordHandler(tornado.web.RequestHandler):
-        def get(self):
-                self.set_header('Content-Type', 'application/json')
-                self.set_header('Access-Control-Allow-Origin', '*')
-                self.write(json.dumps(words))
+	def get(self):
+		self.set_header('Content-Type', 'application/json')
+		self.set_header('Access-Control-Allow-Origin', '*')
+		self.write(json.dumps(words))
 
 application = tornado.web.Application([
-        (r"/data", DataHandler),
-        (r"/score", ScoringHandler),
-        (r"/words", WordHandler),
-        (r"/bower_components/(.*)", tornado.web.StaticFileHandler, { 'path': os.path.join(os.getcwd(), 'bower_components') }),
-        (r"/(.*)", tornado.web.StaticFileHandler, { 'path': os.path.join(os.getcwd(), 'app') })
+	(r"/data", DataHandler),
+	(r"/score", ScoringHandler),
+	(r"/words", WordHandler),
+	(r"/bower_components/(.*)", tornado.web.StaticFileHandler, { 'path': os.path.join(os.getcwd(), 'bower_components') }),
+	(r"/(.*)", tornado.web.StaticFileHandler, { 'path': os.path.join(os.getcwd(), 'app') })
 ])
 
 if __name__ == "__main__":
-        print("Fetching")
-        X, y_score, attributeNames, count, authorNames, subredditNames, words = reddit_info(0)
+	print("Fetching")
+	startTime = datetime.now()
+	X, y_score, attributeNames, count, authorNames, subredditNames, words = reddit_info(0)
+	print("Fetched in " + str(datetime.now()-startTime))
 
-        attributeNames = ["gilded", "downs", "edited", "commentLength", "sentimentScore", "controversiality",
-                              "authorNameLength", "archived", "timestamp", "tod", "dow", "hod","thread score","external link", "author", "subreddit"]
-        attributeNamesShort = ["g", "d", "e", "cL", "sS", "c",
-                              "aNL", "a", "t", "tod", "dow", "hod","ts","el","an","s"]
+	attributeNames = ["gilded", "downs", "edited", "commentLength", "sentimentScore", "controversiality",
+							"authorNameLength", "archived", "timestamp", "tod", "dow", "hod","thread score","external link", "author", "subreddit"]
+	attributeNamesShort = ["g", "d", "e", "cL", "sS", "c",
+							"aNL", "a", "t", "tod", "dow", "hod","ts","el","an","s"]
 
-        print("Transforming")
-        comments = [dict(zip(attributeNamesShort, record)) for record in X]
-        for i in range(len(y_score)):
-                comments[i]["an"] = authorNames[i]
-                comments[i]["s"] = subredditNames[i]
-        print("Server listening")
-        application.listen(8888)
-        tornado.ioloop.IOLoop.instance().start()
+	print("Transforming")
+	comments = [dict(zip(attributeNamesShort, record)) for record in X]
+	for i in range(len(y_score)):
+		comments[i]["an"] = authorNames[i]
+		comments[i]["s"] = subredditNames[i]
+
+	print("Building predictor")
+	startTime = datetime.now()
+	# Shave off to fit with stuff aviable at page
+	X_shave = np.delete(X, np.c_[[0,1,2,5,7,14,15,16]], 1)
+
+	y_binned = [0] * len(y_score)
+	for i in range(len(y_score)):
+		if y_score[i] < 5:
+			y_binned[i] = 0
+		else:
+			y_binned[i] = 1
+
+	# Optimal model
+	clfrand = RandomForestClassifier(n_estimators = 400 , max_features=7, bootstrap=True, oob_score=True)
+	clfrand.fit(X_shave, y_binned)
+	print("Built in " + str(datetime.now()-startTime))
+
+	print("Server listening")
+	application.listen(8888)
+	tornado.ioloop.IOLoop.instance().start()
 
